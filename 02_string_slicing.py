b891b594b5e4aa6838f6f9cf17660dfc716c7fc3
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-12 17:00:11 +0530

    string slicing

---
+++ 02_string_slicing.py
@@ -0,0 +1,9 @@
+# greeting= "Good morning, "
+# name= "Sharma "
+# print(type(name))
+# # Concatenating two strings
+# print( greeting+name)
+name= "Sharma "
+print(name[3])
+# name[3]= "d" --> Does not work
+print(name[0:3])
