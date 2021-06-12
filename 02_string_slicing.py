2f45fafbc77e76089dba073824eb948a6f1bb0af
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-12 19:50:27 +0530

    Practice of String Slicing0

--- 02_string_slicing.py
+++ 02_string_slicing.py
@@ -3,10 +3,23 @@
 # print(type(name))
 # # Concatenating two strings
 # print( greeting+name)
-name= "Sharma "
-print(name[3])
-# name[3]= "d" --> Does not work
-print(name[0:3])
-print(name[:3]) # is as same as print(name[0:3])
-print(name[0:]) # is as same as print(name)  , means, prints full
-print(name[-1:-5])
+name= "Sharma"
+# print(name[3])
+# # name[3]= "d" --> Does not work
+# print(name[0:3])
+# print(name[:3]) # is as same as print(name[0:3])
+# print(name[0:]) # is as same as print(name)  , means, prints full
+# print(name[-1:-5]) 
+
+# d = name[1:4:1] # with a skip value of 1 ---> every 1st charcater selected
+# print(d)
+
+# d1 = name[1:4:2] # with a skip value of 2 ---> every 2nd charcater selected
+# print(d1)
+
+name1= "SharmaIsNice"
+name2= "0123456789"
+e= name2[0:12:2] #----> all numbers divisible by 2
+print(e)
+f= name2[0:10:3] #----> all numbers divisible by 3
+print(f)
