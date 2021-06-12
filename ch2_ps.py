d430306cd6987de0f1b43dabac7a3f8809b3ee5e
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-12 13:13:48 +0530

    Practice

---
+++ ch2_ps.py
@@ -0,0 +1,15 @@
+# ques. 1
+a = 34
+b = 12
+print("The sum of a and b is", a+b)
+
+# ques. 2
+f= 30
+q= 11
+print("The quotient when f is divided by q is", f/q , 
+      "The remainder when f is divided by q is", f%q)
+
+# ques. 3
+d= input("What is your name?")
+d= int(d) #if possible
+print(type(d))
