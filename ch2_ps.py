f62b320fa8c0f2810ac9ee8d2a4bda528af49548
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-12 16:29:54 +0530

    Python practice

--- ch2_ps.py
+++ ch2_ps.py
@@ -11,5 +11,21 @@ print("The quotient when f is divided by q is", f/q ,
 
 # ques. 3
 d= input("What is your name?")
-d= int(d) #if possible
+# d= int(d) #if possible
 print(type(d))
+
+# ques. 4
+a= 34
+b= 80
+c= (a>b)
+print("lets see if you are correct",c)
+
+# ques. 5
+a2= input("Enter no. 1 : ")
+a3= input("Enter no. 2 : ")
+a2= int(a2)
+a3= int(a3)
+avg=(a2 + a3)/2
+print("The average of the two numbers you entered is", avg)
+
+
