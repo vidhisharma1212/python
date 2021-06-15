682283c8c8ac3894f90d1770d0afdb1cb5e405df
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-15 20:43:48 +0530

    lists

---
+++ ch4_lists.py
@@ -0,0 +1,10 @@
+a_list = [1, 6, 94, 0.02, 0, 67]
+a_list[0]= 98
+print(a_list)
+
+jim= [3, "Programming", 3.2, False]
+print(jim)
+
+subjects= ["Physics", "Chemistry", "Maths", "Social Science", "Physical Education"]
+print(subjects[0:4])
+print(subjects[-2:])
