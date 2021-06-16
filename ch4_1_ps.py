aeb6cc2dfd2552f453e51cbbb6123af6856fd174
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-16 16:23:34 +0530

    Lists and Tuples

---
+++ ch4_1_ps.py
@@ -0,0 +1,10 @@
+f1= input("Enter Fruit 1 here : ")
+f2= input("Enter Fruit 2 here : ")
+f3= input("Enter Fruit 3 here : ")
+f4= input("Enter Fruit 4 here : ")
+f5= input("Enter Fruit 5 here : ")
+f6= input("Enter Fruit 6 here : ")
+f7= input("Enter Fruit 7 here : ")
+
+myfruitList= [f1,f2,f3,f4,f5,f6,f7]
+print(myfruitList)
---
+++ ch4_2_ps.py
@@ -0,0 +1,10 @@
+f1= int(input("Enter Marks of Student 1 here : "))
+f2= int(input("Enter Marks of Student 2 here : "))
+f3= int(input("Enter Marks of Student 3 here : "))
+f4= int(input("Enter Marks of Student 4 here : "))
+f5= int(input("Enter Marks of Student 5 here : "))
+f6= int(input("Enter Marks of Student 6 here : "))
+
+ScoreList= [f1,f2,f3,f4,f5,f6]
+ScoreList.sort()
+print(ScoreList)
