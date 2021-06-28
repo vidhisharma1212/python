53a3e402ef1f58184370c4961e56601280e2f18f
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-28 18:21:30 +0530

    Loops practice in python

---
+++ ch7_01_ps.py
@@ -0,0 +1,13 @@
+n= int(input("Enter a number here : \n"))
+for i in range(1,11):
+    # print(str(n)+ "X" + str(i) + "=" + str(i*n) )
+    print(f"{n}X{i}={n*i}")
+
+
+
+
+
+# i=0
+# while n*i<=n*10 :
+#     print(n*i)
+#     i=i+1
---
+++ ch7_02_ps.py
@@ -0,0 +1,4 @@
+l1= ["one", "sonal", "sohan", "titu", "shirley"]
+for name in l1 :
+    if name.startswith("s"):
+        print("Hello, "+ name +"!")
