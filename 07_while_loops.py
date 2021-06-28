9f2af4acdc074f8369a391c8b0033b6b7da8696e
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-28 17:24:47 +0530

    While loops

---
+++ 07_while_loops.py
@@ -0,0 +1,15 @@
+# i= 0
+# while i<10:
+#     print("Yes" + str(i))
+#     i=i+1
+
+# n= 1
+# while n<=50:
+#     print(str(n))
+#     n=n+1
+    
+l= ['mango', 'apple', 'kiwi', 'guava', 'watermelon']
+c=0
+while c<len(l):
+    print(l[c])
+    c=c+1
