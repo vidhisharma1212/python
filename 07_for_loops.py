8cfe5bd95b7a0e18abafd3dfc4f5969840626582
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-28 17:38:50 +0530

    For and While loops Python

---
+++ 07_for_loops.py
@@ -0,0 +1,7 @@
+l= ['mango', 'apple', 'kiwi', 'guava', 'watermelon']
+
+for item in l :
+    print(item)
+
+numbers= [2<numbers<10]
+print(type(numbers))
---
+++ 07_range_function.py
@@ -0,0 +1,10 @@
+
+# (start.stop,step size)
+
+for i in range (8):   # (8) by default means (0,8), or , starts from 0... till 8th number, i.e 7
+    print(i)
+for k in range (1,8):   # it starts from 1... till 8th number, i.e 8
+    print(k)
+# step size is the 'd' value as in arithmetic progression 
+for i in range (1,8,2):   # (8) by default means (0,8), or , starts from 0... till 8th number, i.e 7
+    print(i)
