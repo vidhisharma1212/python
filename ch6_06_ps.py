0c43fc94566ba2a6b79997814e5a17a5452956cc
Vidhi Sharma <vidhisharma921@gmail.com>
2021-06-28 17:02:18 +0530

    Conditionals in python complete

--- ch6_06_ps.py
+++ ch6_06_ps.py
@@ -1,14 +1,30 @@
 x= int(input("Enter your grades here : \n"))
 
-if 90<x<100 :
-    print("Grade: Excellent")
-elif 80<x<90 :
-    print("Grade: A")
-elif 70<x<80 :
-    print("Grade: B")
-elif 60<x<70 :
-    print("Grade: C")
-elif 50<x<60 :
-    print("Grade: D")
+# if 90<=x<=100 :
+#     print("Grade: Excellent")
+# elif 80<=x<90 :
+#     print("Grade: A")
+# elif 70<=x<80 :
+#     print("Grade: B")
+# elif 60<=x<70 :
+#     print("Grade: C")
+# elif 50<=x<60 :
+#     print("Grade: D")
+# else :
+#     print("Grade: F")
+
+# Now, other method can be-
+if x>=90 :
+    grade='Ex'
+elif x>=80 :
+    grade='A'
+elif x>=70 :
+    grade='B'
+elif x>=60 :
+    grade='C'
+elif x>=50 :
+    grade='D'
 else :
-    print("Grade: F")
+    grade='F'
+print("Your grade is : " + grade)
+print("Your grade is : ",grade)
---
+++ ch6_07_ps.py
@@ -0,0 +1,8 @@
+post= input("Enter a post here : \n")
+
+if "harry" in post :
+    print("yes! it is talking about harry")
+elif "Harry" in post :
+     print("yes! it is talking about harry")
+else:
+    print("no, harry not in post")
