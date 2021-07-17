2e4999022e6bfe442b1e66b4ff2d6a5719dc8502
Vidhi Sharma <vidhisharma921@gmail.com>
2021-07-17 09:37:18 +0530

    Files in Python

--- c9/multiplication table/Multiplication.py
+++ c9/multiplication table/Multiplication.py
@@ -2,5 +2,7 @@ for i in range(2,20):
     f=open(f"multiplications_table_of_{i}", "w")
     for k in range(1,11):
             f.write(f"{i}X{k}={i*k}")
+            if k!=10:
+                f.write("\n")
     break
 f.close()
